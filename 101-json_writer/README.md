2c45a7aa921b0587d11d202f6e89636643feb650
E7EE477C11C98D403F318118733EAA5C <E7EE477C11C98D403F318118733EAA5C@i-search.com.cn>
2022-08-23 10:49:15 +0800

    feat: 添加 101-json_writer 示例

---
+++ 101-json_writer/README.md
@@ -0,0 +1,13 @@
+# 保存为json文件示例
+
+    # 创建工程
+    scrapy startproject spider .
+
+    # 启动爬虫
+    scrapy crawl bilibili
+
+
+## 参考
+
+- https://docs.scrapy.org/en/latest/topics/item-pipeline.html#write-items-to-a-json-file
+
---
+++ 101-json_writer/spider/spiders/bilibili.py
@@ -0,0 +1,61 @@
+import scrapy
+import pprint
+import json
+from scrapy.exceptions import CloseSpider
+from ..items import AnimeItem
+
+
+class BilibiliSpider(scrapy.Spider):
+    name = 'bilibili'
+    # start_urls = ['http://bilibili.com/']
+
+    custom_settings = {
+        'COOKIES_ENABLED': False,
+        'DOWNLOADER_DELAY': 10.3,
+    }
+
+    # 番剧索引
+    index_url = 'https://api.bilibili.com/pgc/season/index/result?season_version=1&spoken_language_type=-1&area=-1&is_finish=-1&copyright=-1&season_status=1&season_month=-1&year=-1&style_id=-1&order=3&st=1&sort=0&page={page}&season_type=1&pagesize={pagesize}&type=1'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.params = {
+            'page': getattr(self, 'page', None) or 1,
+            'pagesize': getattr(self, 'pagesize', None) or 20,
+        }
+
+    def start_requests(self):
+        urls = [
+            self.index_url.format(**self.params)
+        ]
+        for url in urls:
+            yield scrapy.Request(
+                url=url,
+                callback=self.parse_index
+            )
+
+    def parse_index(self, response):
+        """
+        @url https://api.bilibili.com/pgc/season/index/result?season_version=1&spoken_language_type=-1&area=-1&is_finish=-1&copyright=-1&season_status=1&season_month=-1&year=-1&style_id=-1&order=3&st=1&sort=0&page=1&season_type=1&pagesize=20&type=1
+        @returns items 1
+        """
+        try:
+            jsondata = response.json()
+        except Exception:
+            raise CloseSpider('解析json失败')
+
+        if 'data' not in jsondata:
+            raise CloseSpider('json没有数据')
+
+        data = jsondata['data']
+        if 'list' in data:
+            _list = data['list']
+            for item in _list:
+                yield AnimeItem(**item)
+
+        # if 'has_next' in data and data['has_next'] == 1:
+        #     self.params['page'] += 1
+        #     yield scrapy.Request(
+        #         url=self.index_url.format(**self.params),
+        #         callback=self.parse_index
+        #     )
